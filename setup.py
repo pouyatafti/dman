e39a5abd4b1d84328be8efdc33d93018def4ff6d
pouya <pouya@nohup.io>
2018-12-16 16:57:42 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dman',
+    version='0.1.0',
+    packages=find_packages(),
+    license='BSD 2-clause "Simplified" License',
+    install_requires=[
+    ],
+)
